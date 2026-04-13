67709d5b57cbcaafb78656f3c47ebadce9a58e63
slothgina <gina.slothstudio@zohomail.com>
2026-04-12 23:43:09 -0400

    got it done finally

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my first app!!!");
+            Console.WriteLine("What do you think about my first app?");
+
+            string response = Console.ReadLine();
+
+            Console.WriteLine("Thanks for your feedback: " + response); 
+
+            Console.WriteLine("\nPress any Key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+}
