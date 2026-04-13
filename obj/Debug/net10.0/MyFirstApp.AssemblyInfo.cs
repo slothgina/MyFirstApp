13d23947dbdfe647e59d1e4d9c480cdac9025805
slothgina <gina.slothstudio@zohomail.com>
2026-04-13 14:13:37 -0400

    its done

--- obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e06d726a0c17bbdb2abfad61be3a465eae1c7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
